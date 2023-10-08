33256dabf210d461a6f2021c12e7ccc6e7f19cdc
Sergey <serg_am@live.ru>
2023-10-08 13:40:51 +0300

    Добавлен сервис

--- src/main/java/com/example/demo/controller/ModifyController.java
+++ src/main/java/com/example/demo/controller/ModifyController.java
@@ -1,67 +1,35 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.ExampleEntity;
-import com.example.demo.repository.ExampleRepository;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.service.ModifyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
 
 @RestController
 @RequestMapping("/modify")
 public class ModifyController {
 
-    private final ExampleRepository exampleRepository;
-    private final ObjectMapper objectMapper;
+    private final ModifyService modifyService;
 
-    @Autowired
-    public ModifyController(ExampleRepository exampleRepository, ObjectMapper objectMapper) {
-        this.exampleRepository = exampleRepository;
-        this.objectMapper = objectMapper;
+    public ModifyController(ModifyService modifyService) {
+        this.modifyService = modifyService;
     }
 
     @PostMapping
-    @Transactional
     public ResponseEntity<ModifyResponse> modifyValue(@RequestBody ModifyRequest request) {
         try {
             Long id = request.getId();
             Integer add = request.getAdd();
 
-            ExampleEntity entity = exampleRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException("Entity not found"));
+            ResponseEntity<ModifyResponse> response = modifyService.modifyValue(id, add);
 
-            String jsonString = entity.getObj();
-
-            // Преобразовать JSON-строку из obj в объект JsonNode
-            JsonNode jsonNode = objectMapper.readTree(jsonString);
-
-            // Извлечь значение поля "current" как Integer
-            System.out.println(jsonNode.get("current").asInt());
-            int current = jsonNode.get("current").asInt();
-
-            current += add;
-
-            // Создать обновленный объект JSON
-            ObjectNode updatedObj = objectMapper.createObjectNode();
-            updatedObj.put("current", current);
-
-            // Преобразовать обновленный объект JSON в строку и сохранить в поле obj
-            entity.setObj(updatedObj.toString());
-
-            // Сохранить обновленное значение obj в базе данных
-            exampleRepository.save(entity);
-
-            return ResponseEntity.ok(new ModifyResponse(current));
+            return response;
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }
+
 
 
 
---
+++ src/main/java/com/example/demo/service/ModifyService.java
@@ -0,0 +1,57 @@
+package com.example.demo.service;
+
+import com.example.demo.controller.ModifyResponse;
+import com.example.demo.entity.ExampleEntity;
+import com.example.demo.repository.ExampleRepository;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ModifyService {
+
+    private final ExampleRepository exampleRepository;
+    private final ObjectMapper objectMapper;
+
+    public ModifyService(ExampleRepository exampleRepository, ObjectMapper objectMapper) {
+        this.exampleRepository = exampleRepository;
+        this.objectMapper = objectMapper;
+    }
+
+    @Transactional
+    public ResponseEntity<ModifyResponse> modifyValue(Long id, Integer add) {
+        try {
+            ExampleEntity entity = exampleRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Entity not found"));
+
+            String jsonString = entity.getObj();
+
+            // Преобразовать JSON-строку из obj в объект JsonNode
+            JsonNode jsonNode = objectMapper.readTree(jsonString);
+
+            // Извлечь значение поля "current" как Integer
+            int current = jsonNode.get("current").asInt();
+
+            current += add;
+
+            // Создать обновленный объект JSON
+            ObjectNode updatedObj = objectMapper.createObjectNode();
+            updatedObj.put("current", current);
+
+            // Преобразовать обновленный объект JSON в строку и сохранить в поле obj
+            entity.setObj(updatedObj.toString());
+
+            // Сохранить обновленное значение obj в базе данных
+            exampleRepository.save(entity);
+
+            return ResponseEntity.ok(new ModifyResponse(current));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.I_AM_A_TEAPOT).build();
+        }
+    }
+}
+
